f2df596d700a1fdc92f4c277bd8d6995933cb73b
Dusty <dustymccord@gmail.com>
2020-04-06 11:59:02 -0700

    add views for profie creation and login redirection.
    Co-authored-by: Michelle Morin <morin2m@gmail.com>
    Co-authored-by: Patrick Kille <59419677+PRKille@users.noreply.github.com>
    Co-authored-by: Jamison Cozart <jamisoncozart@gmail.com>

--- TreasureSweep/Controllers/ProfilesController.cs
+++ TreasureSweep/Controllers/ProfilesController.cs
@@ -24,9 +24,20 @@ namespace TeasureSweepGame.Controllers
       _userManager = userManager;
     }
 
-    public ActionResult Index()
+    public async Task<ActionResult> Index()
     {
-      return View();
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      try
+      {
+        Profile thisProfile = _db.Profiles.FirstOrDefault(profile => profile.User == currentUser);
+        thisProfile.Games = _db.Games.Where(game => game.P1Id == thisProfile.ProfileId || game.P2Id == thisProfile.ProfileId).ToList();
+        return View(thisProfile);
+      }
+      catch (Exception)
+      {
+        return RedirectToAction("Create");
+      }
     }
 
     public ActionResult Create()
@@ -44,13 +55,6 @@ namespace TeasureSweepGame.Controllers
       _db.Profiles.Add(profile);
       _db.SaveChanges();
       return RedirectToAction("Index");
-    }
-
-    public ActionResult Details(int id)
-    {
-      Profile thisProfile = _db.Profiles.FirstOrDefault(profile => profile.ProfileId == id);
-      ViewBag.Games = _db.Games.Where(game => game.P1Id == id || game.P2Id == id).ToList();
-      return View(thisProfile);
     }
 
     public ActionResult Edit(int id)
